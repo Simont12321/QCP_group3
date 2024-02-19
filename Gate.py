f052da717749c4dd82bfc562b06f6814c053298d
Tracque <48903708+Tracque@users.noreply.github.com>
2024-02-19 12:22:09 +0000

    Update Gate.py

--- Gate.py
+++ Gate.py
@@ -1,67 +1,86 @@
 import numpy as np
-#import SparseMatrix
-import Dense
-#import LazyMatrix
+from Sparse import SparseMatrix
+from Dense import DenseMatrix
+from Lazy import LazyMatrix
 
 class Gate(object):
     
-    def __init__(self, matrixType, quBitOne, gateName, quBitTwo = None, customInput = None):
+    def __init__(self, matrixType, quBits, gateName = None, customInput = None):
     
-        #Store the one (or two) quBits we're acting on
-        self.quBitOne = quBitOne
-        self.quBitTwo = quBitTwo
-        
-        self.gateName = gateName
+        #Store the quBits we're acting on
+        self.quBits = quBits
         self.customInput = customInput
         
-        if matrixType == "Dense":
-            self.matrixType = DenseMatrix
-       # elif matrixType == "Sparse":
-       #     self.matrixType = SparseMatrix
-       # elif matrixType == "Lazy":
-       #     self.matrixType = LazyMatrix
+        self.matrixType = matrixType
+            
+        self.getGate(gateName)    
         
-        self.Gate = self.gateName()
+        if self.customInput != None:
+            self.gateMethod = self.custom
+            
+        self.Gate = self.gateMethod()
         
+    def getGate(self, gateName):
+        
+        if gateName == "hadamard":
+            self.gateMethod = self.hadamard
+        elif gateName == "cNot":
+            self.gateMethod = self.cNot
+        elif gateName == "cV":
+            self.gateMethod = self.cV
+        elif gateName == "spinX":
+            self.gateMethod = self.spinX
+        elif gateName == "spinY":
+            self.gateMethod = self.spinY
+        elif gateName == "spinZ":
+            self.gateMethod = self.spinZ
     
     def hadamard(self):
         
         hadamard = 1/np.sqrt(2) * np.array([[1,1],[1,-1]])
         
-        return self.matrixType(hadamard)
+        return self.makeGate(hadamard)
     
     def cNot(self):
         
         cNot = np.array([1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0])
         
-        return self.matrixType(cNot)
+        return self.makeGate(cNot)
     
     def cV(self):
         
         cV = np.array([1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1j])
         
-        return self.matrixType(cV)
+        return self.makeGate(cV)
     
     def custom(self):
         
         custom = np.array(self.customInput)
         
-        return self.matrixType(custom)
+        return self.makeGate(custom)
     
     def spinX(self):
         
         spinX = np.array([0,1],[1,0])
         
-        return self.matrixType(spinX)
+        return self.makeGate(spinX)
     
     def spinY(self):
         
         spinY = np.array([0,-1j],[1j,0])
         
-        return self.matrixType(spinY)
+        return self.makeGate(spinY)
     
     def spinZ(self):
         
         spinZ = np.array([1,1],[0,-1])
         
-        return self.matrixType(spinZ)
+        return self.makeGate(spinZ)
+    
+    def makeGate(self, matrix):
+        
+        self.Gate = DenseMatrix(matrix)
+        
+        if self.matrixType == "Sparse":
+            self.Gate.Sparse()
+        
