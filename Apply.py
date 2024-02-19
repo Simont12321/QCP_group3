d4cef8e6b525bc3cc6cc6b6c909047381bc5ab62
Gekbako <jakub.gregus@gmail.com>
2024-02-19 03:32:22 +0000

    qReg update

--- Apply.py
+++ Apply.py
@@ -3,8 +3,10 @@
 # import SparseMatrix
 from time import time
 import numpy as np
+
+
 class Apply(object):
-    def __init__(self, VectorApply, ApplyType = None):
+    def __init__(self, VectorApply, ApplyType=None):
         # Check correct format
         assert isinstance(VectorApply, list)
         self.VectorApply = VectorApply
@@ -34,14 +36,13 @@ class Apply(object):
 
         # Loop over matrix elements and vector elements to find new vector elements and append them
 
-        for i in range(0,len(Matrix)):
+        for i in range(0, len(Matrix)):
             NewVectorelem = 0
-            for j in range(0,len(Matrix[i])):
+            for j in range(0, len(Matrix[i])):
                 NewVectorelem += Matrix[i][j]*Vector[j]
             NewVector.append(NewVectorelem)
-        NewVector = np.asarray(NewVector, dtype = complex)
-        return(NewVector)
-    
+        NewVector = np.asarray(NewVector, dtype=complex)
+        return (NewVector)
 
     def SparseApply(self):
         """
@@ -59,32 +60,23 @@ class Apply(object):
         The output will be a vector itself
         """
     # Start product
-        
+
         Matrix = self.VectorApply[0]
         Vector = self.VectorApply[1]
-        NewVector = np.zeros_like(Vector, dtype = complex)
+        NewVector = np.zeros_like(Vector, dtype=complex)
 
     # Loop over matrix elements and vector elements to add to NewVector
 
         for elem in Matrix:
             NewVector[elem[0]] += (elem[2])*(Vector[elem[1]])
-        return(NewVector)
+        return (NewVector)
 
 
-
-
-MDense = np.array([[3,0,0],[0,0,0],[0,0,6]])
+"""MDense = np.array([[3,0,0],[0,0,0],[0,0,6]])
 MSparse = np.array([[0,0,3],[2,2,6]])
 u = np.array([1,2,3])
 MDenseApplyu = Apply([MDense,u])
 MSparseApplyu = Apply([MSparse,u])
 print(MDenseApplyu.DenseApply())
 print(MSparseApplyu.SparseApply())
-
-
-
-
-
-
-
-    
+"""
--- Q_Register.py
+++ Q_Register.py
@@ -1,22 +1,75 @@
 import numpy as np
+import Apply
 
 H_gate = 1/np.sqrt(2) * np.array([[1, 1], [1, -1]])
 
 
+class Qubit:
+    def __init__(self, state=np.array([1, 0], dtype=complex)) -> None:
+
+        self.state = np.array(self.normalize(state), dtype=complex)
+
+    def normalize(self, vec_to_norm):
+        """
+        Normalizes a complex vector to magnitude 1
+
+        Args:
+        vec_to_norm (1D array) : 2*1 vector holding the linear coefficient of "unormalized" state
+        """
+
+        assert (isinstance(vec_to_norm, np.ndarray) and len(
+            vec_to_norm) == 2), "Supply complex vector (2*1 numpy array)"
+
+        factor = np.sqrt((vec_to_norm*np.conj(vec_to_norm)).sum())
+        return vec_to_norm / factor
+
+    def apply(self, gate):
+        """
+        Applies a gate to a qubit -> performs inner profuct on the 
+        matrix and vector representation of the state of the qubit
+
+        Args:
+        gate (2D array) : square matrix repesentig a quantum gate (lin operator)
+        """
+
+        temp = Apply([gate, self.state])
+        self.state = temp.DenseApply()
+
+    def measure(self):
+        """
+        A qubit is of form |state> = a|+> + b|->, so when measured,
+        based on its amplitudes (a,b) it will collapse either to |+> or |->
+        """
+
+        P = np.abs(self.state) ** 2
+        pos = np.random.choice([0, 1], p=P)
+        self.state[pos] = 1
+        self.state[pos-1] = 0
+
+    def __str__(self) -> str:
+        out = f"state = | {self.state[0]}|+> + {self.state[1]}|-> >"
+        return out
+
+
 class Q_Register:
 
-    def __init__(self, n: int) -> None:
+    def __init__(self, n: int, states=None) -> None:
         """
         Initializes the Q_Register with n |0> state Qubits
 
         Args:
         n (int) : number of qubits
         """
-        self.n = n
-        # default |0> state
-        qubit = np.array([1, 0])
-        # initialize n qubits
-        self.states = np.tile(qubit, 2**(n-1)).reshape((2,)*n)
+        temp = []
+        if np.all(states) == None:
+            for i in range(n):
+                temp.append(Qubit())
+        else:
+            for i in range(n):
+
+                temp.append(Qubit(states[2*i: 2*(i+1)]))
+
+        self.qubits = np.array(temp)
 
     def apply_gate(self, gate, index):
         """
@@ -33,15 +86,24 @@ class Q_Register:
 
     def measure(self):
         """
-        Measurement of the Q_Register, all the qubits collapse into classical bit (0 or 1)
-
-        Returns:
-        result (binary reresentation of int) : the current state of register
+        Measurement of the Q_Register, all the qubits collapse into |+> or |->        
         """
-        probabilities = [abs(q)**2 for q in self.states.flatten()]
-        result = np.random.choice(range(self.n), 1, p=probabilities)
-        return bin(result)
+
+        for q in self.qubits:
+            q.measure()
 
     def __str__(self) -> str:
         # prints the Q_Register as 1D array
-        print(f"{self.states.flatten()}")
+        out = f""
+        for x in self.qubits:
+            out += f"{x.state}"
+        return out.replace(" ", ", ").replace("][", ", ")
+
+
+a = np.array([1+1j, 2+2j], dtype=complex)
+b = np.array([3+3j, 4+4j], dtype=complex)
+q = Q_Register(4, np.array([1.+1.j, 1.+1.j, 2.+2.j, 2.+2.j,
+               3.+3.j, 3.+3.j, 4.+4.j, 4.+4.j]))
+print(q)
+q.measure()
+print(q)
