efbca7b27f5790736eea9855bd7089086e0c7d41
Simont12321 <swimon316@gmail.com>
2024-02-23 16:53:34 +0000

    added measurements

--- test_circuit.py
+++ test_circuit.py
@@ -9,10 +9,16 @@ from Tensor import TensorProduct
 
 # initialize a 2 qbit register 
 register1 = Q_Register(2)
-#print(f"qubits are {register1}.")
+
+print(f"Initial qubits are {register1}.")
 
 # create a hadamard gate to act on register1
-gate1 = Gate(DenseMatrix, register1, "hadamard") 
+gate1 = Gate("Dense", "hadamard") 
 
-# apply the hadamard gate to register1
-register1.apply_gate(gate1) 
+# # apply the hadamard gate to register1
+register1.apply_gate(gate1,[0,1]) 
+print(f"Qubits after the gate are {register1}.")
+
+# measure the register
+register1.measure()
+print(f"Collapsed qubits are {register1}.")
